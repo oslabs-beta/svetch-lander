0f7c2351a9af3acae37f9fb50a0d52f9f115ea7b
DRPalumbo17 <DRPalumbo17@gmail.com>
2022-04-26 19:51:29 -0400

    first commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,12 @@
 <script>
-</script>
-
-<main>
-	<h1>A SHINY NEW APP</h1>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-	h1 {
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+	import Canvas from './lib/Canvas.svelte'
+  </script>
+  
+  <main>
+	<Canvas/>
+   
+  </main>
+  
+  <style>
+   
+  </style>
---
+++ src/lib/Canvas.svelte
@@ -0,0 +1,16 @@
+<div id = "Canvas">
+    filler text
+</div>
+
+<style>
+    #Canvas
+    {
+        border: 2px solid black;
+        height: 40em;
+        width: 80%;
+        margin-left: 20%;
+        border-radius: 15px;
+        background-color: aqua;
+
+    }
+</style>
