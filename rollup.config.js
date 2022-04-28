4874d6aeb37cd5716319bf70022e4b40e02e86e0
ekathuria <e@ekathuria.dev>
2022-04-28 11:45:12 -0400

    Scaffold codeblock component

--- rollup.config.js
+++ rollup.config.js
@@ -22,7 +22,8 @@ export default {
 			// a separate file - better for performance
 			css: css => {
 				css.write('public/build/bundle.css');
-			}
+			},
+			emitCss: false,
 		}),
 
 		// If you have external dependencies installed from
--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,12 @@
 <script>
+	import Codeblock from './Codeblock.svelte' ;
 </script>
 
-<main>
-	<h1>A SHINY NEW APP</h1>
-</main>
+<pre>
+	<code>
+		<Codeblock />
+	</code>
+</pre>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-	h1 {
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
